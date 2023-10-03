e29c9a4b8fb72c86511cab8c635f142170354c1a
Raaed-Mirza <raaedmirza@gmail.com>
2023-10-02 21:56:38 -0400

    ssdf

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/process', methods=['POST'])
+def process():
+    data = request.get_json()
+    name = data['name']
+    email = data['email']
+
+    # Process the data or perform any backend tasks
+    # ...
+
+    # Return a response back to the frontend
+    response = {
+        "status": "success",
+        "message": "Data received successfully!"
+    }
+
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run()
